91334ae355620523a5d3ebc4a327b5808a198c54
Simon Ruzicka <ruzicsi1@fit.cvut.cz>
2025-01-21 12:41:30 +0100

    feat: New copied axi_stream_driver

--- hls4ml/templates/vivado_accelerator/alveo/python_drivers/axi_stream_driver.py
+++ hls4ml/templates/vivado_accelerator/alveo/python_drivers/axi_stream_driver.py
@@ -9,6 +9,8 @@ class NeuralNetworkOverlay(Overlay):
         super().__init__(xclbin_name, dtbo=dtbo, download=download, ignore_version=ignore_version, device=device)
         self.input_buffer = None
         self.output_buffer = None
+        self.weight_buffer = None
+        self.weight_size = None
 
     def allocate_mem(self, X_shape, y_shape, dtype=np.float32, trg_in=None, trg_out=None):
         """Buffer allocation in the accelerator's memory.
@@ -16,6 +18,7 @@ class NeuralNetworkOverlay(Overlay):
         Args:
             X_shape (list): Input buffer shape.
             y_shape (list): Output buffer shape.
+            w_shape (list): Weights buffer shape.
             dtype (dtype, optional): The data type of the elements of the input/output tensors. Must be an instance of
                 numpy dtype. Defaults to np.float32.
 
@@ -41,6 +44,16 @@ class NeuralNetworkOverlay(Overlay):
         """
         self.input_buffer = allocate(shape=X_shape, dtype=dtype, target=trg_in)
         self.output_buffer = allocate(shape=y_shape, dtype=dtype, target=trg_out)
+
+    def fill_weights(self, data: np.ndarray):
+        assert data.dtype == np.int16, "Incorrect data type, stuff may fail"
+        # assert np.prod(data.shape) == CONSTANT_PASSED_FROM_HLS4ML, "Data size must match the buffer size"
+
+        self.weight_size = data.shape
+
+        self.weight_buffer = allocate(shape=data.shape, dtype=data.dtype, target=None)
+        self.weight_buffer[:] = data.flatten()
+        self.weight_buffer.sync_to_device()
 
     def predict(self, X, y_shape, dtype=np.float32, debug=False, profile=False, encode=None, decode=None):
         """Obtain the predictions of the NN implemented in the FPGA.
@@ -71,7 +84,7 @@ class NeuralNetworkOverlay(Overlay):
         self.input_buffer.sync_to_device()
         if debug:
             print("Send OK")
-        self.krnl_rtl_1.call(self.input_buffer, self.output_buffer, in_size, out_size)
+        self.krnl_rtl_1.call(self.input_buffer, self.weight_buffer, self.output_buffer, in_size, self.weight_size, out_size)
         if debug:
             print("Kernel call OK")
         self.output_buffer.sync_from_device()
@@ -99,3 +112,32 @@ class NeuralNetworkOverlay(Overlay):
         print(f"Classified {N} samples in {dts} seconds ({rate} inferences / s)")
         print(f"Or {1 / rate * 1e6} us / inferences")
         return dts, rate
+
+
+if __name__ == "__main__":
+    with open("tb_weights.dat") as f:
+        weights: list[float] = [float(x) for x in f.read().split()]
+    print(weights)
+
+    # assume ap_fixed<16, 6> == 1 bit sign, 6 bit integer part, 9 bit fraction part
+    # numpy doesn't have fixed point numbers, so we use ints
+    fixed_point_convertor = 2 ** 9
+    weights_fixed = np.ndarray(len(weights), np.int16)
+    weights_fixed[:] = [x * fixed_point_convertor for x in weights]
+
+    print(weights_fixed / fixed_point_convertor)
+
+    with open("tb_input_features.dat") as f:
+        # 4 values per row, assume normalized
+        inputs: list[float] = [float(x) for x in f.read().split()]
+
+    inputs_fixed = np.ndarray(len(inputs), np.int16)
+    inputs_fixed[:] = [x * fixed_point_convertor for x in inputs]
+    inputs_fixed = inputs_fixed.reshape(-1, 4)
+
+    # one-hot encoding of 3 classes
+    output_shape = (inputs_fixed.shape[0], 3)
+
+    nn = NeuralNetworkOverlay('iris_hls4ml_prj_kernel.xclbin')
+    nn.fill_weights(weights_fixed)
+    y_hw, latency, throughput = nn.predict(inputs_fixed, output_shape, dtype=np.int16, profile=True)
